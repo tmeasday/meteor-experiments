bad6a66b5737ae475104578c85eb3d124baee5e6
Zoltan Olah <zol@percolatestudio.com>
2014-09-17 11:23:11 -0700

    Experiment 1

--- experiments.js
+++ experiments.js
@@ -1,23 +1,11 @@
+Documents = new Meteor.Collection('documents');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+  Meteor.subscribe('documents');
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish('documents', function() {
+    return Documents.find();
   });
 }
