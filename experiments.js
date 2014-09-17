4899f88672217765c92000a929ab8bf5842954d5
Zoltan Olah <zol@percolatestudio.com>
2014-09-17 14:41:56 -0700

    Experiment 2

--- experiments.js
+++ experiments.js
@@ -2,10 +2,35 @@ Documents = new Meteor.Collection('documents');
 
 if (Meteor.isClient) {
   Meteor.subscribe('documents');
+  
+  UI.registerHelper('debugInfo', function() {
+    var doc = Documents.findOne() || {data: ''};
+    return 'Documents: ' + Documents.find().count() + ' of size: ' + doc.data.length;
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.publish('documents', function() {
     return Documents.find();
+  });
+
+  Meteor.startup(function() {
+    console.log('Building dataset...');
+    //~10k of data by default
+    var num = process.env.DOCUMENT_NUMBER || 200;
+    var size = process.env.DOCUMENT_SIZE || 100;
+    
+    var data = [];
+    _.times(size, function() {
+      data.push('d')
+    });
+    var dataString = data.join('');
+
+    Documents.remove({});
+    _.times(num, function() {
+      Documents.insert({data: dataString});
+    });
+
+    console.log('Built dataset (documents:' + num + ' size: ' + size + ')');
   });
 }
